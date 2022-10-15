c1200707ff1e79e58449abcd7628f642e950e99c
wakeup <caro1xxxhv@gmail.com>
2022-10-15 20:01:29 +0800

    formation

---
+++ frontend/components/globalstyles.tsx
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    color: ${({ theme }) => theme.colors.primary};
+    padding: 0;
+    margin: 0;
+  }
+`;
+
+export default GlobalStyle;
---
+++ frontend/pages/index.tsx
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import NavBar from "../components/NavBar";
+import Search from "../components/Search";
+import PostItem from "../components/PostItem";
+import { useState } from "react";
+import { nanoid } from "nanoid";
+import { postListRes, postFiled } from "../type/post";
+export default function Home({ data }) {
+  const [postList, setPostList] = useState(JSON.parse(data.post));
+  return (
+    <div>
+      <NavBar></NavBar>
+      <Search></Search>
+      <PostItem post={postList}></PostItem>
+    </div>
+  );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`http://127.0.0.1:8000/api/v1/PostList/`);
+  const data: postListRes = await res.json();
+  return { props: { data } };
+}
